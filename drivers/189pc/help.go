257b7411db8d5ed0bd8e0353d28749f5c31486b6
Kopw <eorzing@gmail.com>
2026-01-09 02:08:38 +0800

    fix

--- drivers/189pc/help.go
+++ drivers/189pc/help.go
@@ -195,15 +195,15 @@ func BoolToNumber(b bool) int {
 
 // 计算分片大小
 // 对分片数量有限制
-// 10MIB 20 MIB 999片
-// 50MIB 60MIB 70MIB 80MIB ∞MIB 1999片
+// 2MIB 4MIB 999片
+// 10MIB 20MIB 50MIB+ ∞MIB 1999片
 func partSize(size int64) int64 {
-	const DEFAULT = 1024 * 1024 * 10 // 10MIB
+	const DEFAULT = 1024 * 1024 * 2 // 2MIB - 更小的分片以支持更多并发
 	if size > DEFAULT*2*999 {
 		return int64(math.Max(math.Ceil((float64(size)/1999) /*=单个切片大小*/ /float64(DEFAULT)) /*=倍率*/, 5) * DEFAULT)
 	}
 	if size > DEFAULT*999 {
-		return DEFAULT * 2 // 20MIB
+		return DEFAULT * 2 // 4MIB
 	}
 	return DEFAULT
 }
